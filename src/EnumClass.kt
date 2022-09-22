c023882ffe493883c055f2923d28997da1fecea2
traduongdatnguyen <tddnguyen.20it8@vku.udn.vn>
2022-09-22 15:26:26 +0700

    Create EnumClass.kt

---
+++ src/EnumClass.kt
@@ -0,0 +1,21 @@
+
+interface TestImplement {
+    fun test(a: Int): Int
+}
+
+interface TestImplement2 {
+    fun test2(b: Int): Int
+}
+
+enum class IPL : TestImplement, TestImplement2 {
+    T1 {override fun test(a: Int) = a * a } ,
+    T2 {override fun test(a: Int) = a * a} ;
+    override fun test2(b: Int) = b * b
+}
+fun main(){
+    // Use
+    val ipl = IPL.T1
+    println(ipl.test(2))          // 4
+    val ipl2 = IPL.T2
+    println(ipl2.test2(4))    // 16
+}
